67eff4fcb921ccf65ed8efa73ddc910a4afbdedd
RAHUL <rahulmeetawa@gmail.com>
2023-05-23 14:12:52 +0530

    rendering files

--- src/ReactTopic/02_RenderingElements/RenderingElementsIndex.js
+++ src/ReactTopic/02_RenderingElements/RenderingElementsIndex.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+
+// TODO: IN this file, we have render three components like Header,Content,footer. these types of component we rendering here.
 
 const RenderingElementsIndex = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
 };
 
 export default RenderingElementsIndex;
+
+//What is Components so we read in next section or topic
