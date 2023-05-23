efabdd1a56b7be5ce38efbc69f82fa93a1d3262e
RAHUL <rahulmeetawa@gmail.com>
2023-05-23 13:56:00 +0530

    render the another files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ComponentIndex from "./ReactTopic/02_Components/ComponentIndex";
+import RenderingElementsIndex from "./ReactTopic/02_RenderingElements/RenderingElementsIndex";
 const App = () => {
   return (
     <>
-      <ComponentIndex />
+      <RenderingElementsIndex />
     </>
   );
 };
