1864631ddb756fc85f6a8c8bcdff616afcb084bc
RAHUL <rahulmeetawa@gmail.com>
2023-05-24 19:29:51 +0530

    rename the file

---
+++ src/ReactTopic/05_ClassComponents/01_BasicInClass.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+export default Car;
--- src/ReactTopic/05_ClassComponents/BasicInClass.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BasicInComponent = () => {
-  return <div></div>;
-};
-
-export default BasicInComponent;
