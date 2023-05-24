1ffcf80d149eff094904a4856e2f9f85b7eaade3
RAHUL <rahulmeetawa@gmail.com>
2023-05-24 14:35:48 +0530

    add class component

--- src/ReactTopic/03_Components/Footer.jsx
+++ src/ReactTopic/03_Components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Footer = () => {
-  return (
-    <div>
-      <h3>copyright@2023</h3>
-    </div>
-  );
-};
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>copyright@2023</h3>
+      </div>
+    );
+  }
+}
 
 export default Footer;
