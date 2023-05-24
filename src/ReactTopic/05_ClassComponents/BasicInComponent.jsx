ab6769498fd7246cf480476b016a6cbe9c98bdf5
RAHUL <rahulmeetawa@gmail.com>
2023-05-24 18:01:28 +0530

    add files in a folder

---
+++ src/ReactTopic/05_ClassComponents/BasicInComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BasicInComponent = () => {
+  return <div></div>;
+};
+
+export default BasicInComponent;
---
+++ src/ReactTopic/05_ClassComponents/ClassComponentIndex.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ClassComponentIndex = () => {
+  return <div></div>;
+};
+
+export default ClassComponentIndex;
