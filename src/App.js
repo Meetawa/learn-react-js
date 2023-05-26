0e87107239535a51fb6f40c1da2e24d486a9a51b
RAHUL <rahulmeetawa@gmail.com>
2023-05-26 08:29:14 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import ClassComponentIndex from "./ReactTopic/05_ClassComponents/ClassComponentIndex";
 const App = () => {
-  return (
-    <>
-      <ClassComponentIndex />
-    </>
-  );
+  return <>rahul</>;
 };
 
 export default App;
---
+++ src/ReactTopic/05_Props/readme.md
@@ -0,0 +1,4 @@
+# React Props
+
+- Props are arguments passed into React components.
+- Props are passed to components via HTML attributes.
