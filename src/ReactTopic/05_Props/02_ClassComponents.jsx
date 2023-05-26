fce012303410b26d0c581b939138c7188ec91e78
RAHUL <rahulmeetawa@gmail.com>
2023-05-26 12:45:56 +0530

    add some class

--- src/ReactTopic/05_Props/02_ClassComponents.jsx
+++ src/ReactTopic/05_Props/02_ClassComponents.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+
 class Car1 extends React.Component {
   render() {
-    return <></>;
+    console.log("car1");
+
+    console.log(this.props.carDetails1.name2);
+    return <>{this.props.carDetails1.name2} Car</>;
   }
 }
 class Car2 extends React.Component {
   render() {
-    return <></>;
+    console.log("car2");
+    console.log(this.props);
+
+    return <>{this.props.carDetails1.name3} Car</>;
   }
 }
 
@@ -17,12 +24,12 @@ class Car extends React.Component {
       <>
         <h1>Numbers of Car Color :</h1>
         <ul>
-          <li>{this.props.carDetails.name1} Car !</li>
+          <li>{this.props.carDetails.name1} Car</li>
           <li>
-            <Car1 />
+            <Car1 carDetails1={this.props.carDetails} />
           </li>
           <li>
-            <Car2 />
+            <Car2 carDetails1={this.props.carDetails} />
           </li>
         </ul>
       </>
