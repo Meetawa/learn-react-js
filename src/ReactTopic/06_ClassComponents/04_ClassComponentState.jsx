3b470ad7fe71e1e9ce22b7674679c36da11813be
RAHUL <rahulmeetawa@gmail.com>
2023-05-27 21:33:26 +0530

    add

--- src/ReactTopic/06_ClassComponents/04_ClassComponentState.jsx
+++ src/ReactTopic/06_ClassComponents/04_ClassComponentState.jsx
@@ -23,4 +23,34 @@ class States extends React.Component {
     );
   }
 }
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964,
+    };
+  }
+  changeColor = () => {
+    this.setState({ color: "blue" });
+  };
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button type="button" onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
+  }
+}
 export default States;
+export { Car };
--- src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
+++ src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ClassConstructor from "./02_ClassConstructor";
 import PropsInConstructor from "./03_PropsInConstructor";
-import States from "./04_ClassComponentState";
+import States, { Car } from "./04_ClassComponentState";
 
 const ClassComponentIndex = () => {
   return (
@@ -9,6 +9,7 @@ const ClassComponentIndex = () => {
       <ClassConstructor />
       <PropsInConstructor model="Mustang" />
       <States />
+      <Car />
     </>
   );
 };
