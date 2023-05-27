164b645d36258c644fb73bedeccbdc903c92fda5
RAHUL <rahulmeetawa@gmail.com>
2023-05-27 21:26:13 +0530

    add content in file

--- src/ReactTopic/06_ClassComponents/04_ClassComponentState.jsx
+++ src/ReactTopic/06_ClassComponents/04_ClassComponentState.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
 class States extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964,
+    };
+  }
   render() {
-    return <h1>rahulsaini</h1>;
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+      </div>
+    );
   }
 }
 export default States;
