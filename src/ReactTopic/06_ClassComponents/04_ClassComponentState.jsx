a8a3186ee4a20be73083a2ab0be694924b235b26
RAHUL <rahulmeetawa@gmail.com>
2023-05-27 21:16:58 +0530

    add content in file

---
+++ src/ReactTopic/06_ClassComponents/04_ClassComponentState.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class States extends React.Component {
+  render() {
+    return <h1>rahulsaini</h1>;
+  }
+}
+export default States;
--- src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
+++ src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ClassConstructor from "./02_ClassConstructor";
 import PropsInConstructor from "./03_PropsInConstructor";
+import States from "./04_ClassComponentState";
 
 const ClassComponentIndex = () => {
   return (
     <>
       <ClassConstructor />
       <PropsInConstructor model="Mustang" />
+      <States />
     </>
   );
 };
