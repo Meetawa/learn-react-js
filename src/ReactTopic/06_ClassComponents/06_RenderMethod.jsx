f517f50f14c811102ad698e975f8d94a6b80d9cd
RAHUL <rahulmeetawa@gmail.com>
2023-05-30 12:01:55 +0530

    add rendermethod file

---
+++ src/ReactTopic/06_ClassComponents/06_RenderMethod.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RenderMethod = () => {
+  return <div></div>;
+};
+
+export default RenderMethod;
--- src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
+++ src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
@@ -3,6 +3,7 @@ import ClassConstructor from "./02_ClassConstructor";
 import PropsInConstructor from "./03_PropsInConstructor";
 import States, { Car } from "./04_ClassComponentState";
 import GetDerivedStateFromProps from "./05_GetDerivedStateFromProps";
+import RenderMethod from "./06_RenderMethod";
 
 const ClassComponentIndex = () => {
   return (
@@ -12,6 +13,7 @@ const ClassComponentIndex = () => {
       <States />
       <Car />
       <GetDerivedStateFromProps favcol="yellow" />
+      <RenderMethod />
     </>
   );
 };
