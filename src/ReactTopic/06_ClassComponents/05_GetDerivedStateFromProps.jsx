aa01f4a1afd7bab9f1e29e6c070af912349f2ac8
RAHUL <rahulmeetawa@gmail.com>
2023-05-30 11:42:33 +0530

    add GetDerivedStateFromProps

---
+++ src/ReactTopic/06_ClassComponents/05_GetDerivedStateFromProps.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class GetDerivedStateFromProps extends React.Component {
+  render() {
+    return <h1>Rahul Saini</h1>;
+  }
+}
+
+export default GetDerivedStateFromProps;
--- src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
+++ src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
@@ -2,6 +2,7 @@ import React from "react";
 import ClassConstructor from "./02_ClassConstructor";
 import PropsInConstructor from "./03_PropsInConstructor";
 import States, { Car } from "./04_ClassComponentState";
+import GetDerivedStateFromProps from "./05_GetDerivedStateFromProps";
 
 const ClassComponentIndex = () => {
   return (
@@ -10,6 +11,7 @@ const ClassComponentIndex = () => {
       <PropsInConstructor model="Mustang" />
       <States />
       <Car />
+      <GetDerivedStateFromProps />
     </>
   );
 };
