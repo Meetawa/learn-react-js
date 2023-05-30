ef049727249d22c673a940b0fc00560ab2ef1daf
RAHUL <rahulmeetawa@gmail.com>
2023-05-30 12:54:13 +0530

    add ComponentDidMount

---
+++ src/ReactTopic/06_ClassComponents/07_ComponentDidMount.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ComponentDidMount extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ComponentDidMount;
--- src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
+++ src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
@@ -4,6 +4,7 @@ import PropsInConstructor from "./03_PropsInConstructor";
 import States, { Car } from "./04_ClassComponentState";
 import GetDerivedStateFromProps from "./05_GetDerivedStateFromProps";
 import RenderMethod from "./06_RenderMethod";
+import ComponentDidMount from "./07_ComponentDidMount";
 
 const ClassComponentIndex = () => {
   return (
@@ -14,6 +15,7 @@ const ClassComponentIndex = () => {
       <Car />
       <GetDerivedStateFromProps favcol="yellow" />
       <RenderMethod />
+      <ComponentDidMount />
     </>
   );
 };
