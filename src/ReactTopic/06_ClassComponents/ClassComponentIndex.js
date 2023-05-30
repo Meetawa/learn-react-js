4c9595227a813cc2b8912575daf59f6c5bc307d5
RAHUL <rahulmeetawa@gmail.com>
2023-05-30 19:58:02 +0530

    add life cycle methods

--- src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
+++ src/ReactTopic/06_ClassComponents/ClassComponentIndex.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ClassConstructor from "./01_Mounting/01_ClassConstructor";
+import ClassConstructor from "./LifeCycle_Methods/01_Mounting/01_ClassConstructor";
 import PropsInConstructor from "./03_PropsInConstructor";
 import States, { Car } from "./04_ClassComponentState";
-import GetDerivedStateFromProps from "./01_Mounting/02_GetDerivedStateFromProps";
-import RenderMethod from "./01_Mounting/03_RenderMethod";
-import ComponentDidMount from "./01_Mounting/04_ComponentDidMount";
+import GetDerivedStateFromProps from "./LifeCycle_Methods/01_Mounting/02_GetDerivedStateFromProps";
+import RenderMethod from "./LifeCycle_Methods/01_Mounting/03_RenderMethod";
+import ComponentDidMount from "./LifeCycle_Methods/01_Mounting/04_ComponentDidMount";
 const ClassComponentIndex = () => {
   return (
     <>
