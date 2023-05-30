e4d598d1003ca4ba9b68868a397b56bcae2bd149
RAHUL <rahulmeetawa@gmail.com>
2023-05-30 13:00:13 +0530

    add ComponentDidMount

--- src/ReactTopic/06_ClassComponents/07_ComponentDidMount.jsx
+++ src/ReactTopic/06_ClassComponents/07_ComponentDidMount.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
 class ComponentDidMount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Colorname: "red",
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ Colorname: "blue" });
+    }, 2000);
+  }
   render() {
-    return <div></div>;
+    console.log(this.state);
+    return <div>my fColor Name is {this.state.Colorname}</div>;
   }
 }
 
