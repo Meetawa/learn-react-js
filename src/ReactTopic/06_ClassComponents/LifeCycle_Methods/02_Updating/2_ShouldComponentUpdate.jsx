12ff905155ceebcc0d2dea1dc31d085f42c94112
RAHUL <rahulmeetawa@gmail.com>
2023-05-31 14:29:19 +0530

    add ShouldComponentUpdate

--- src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/2_ShouldComponentUpdate.jsx
+++ src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/2_ShouldComponentUpdate.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
+// In the shouldComponentUpdate() method you can return a Boolean value that specifies whether React should continue with the rendering or not.
 class ShouldComponentUpdate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favoritecolor: "red" };
+  }
+  shouldComponentUpdate() {
+    return true;
+  }
+  changeColor = () => {
+    this.setState({ favoritecolor: "blue" });
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+        <button type="button" onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
   }
 }
 
