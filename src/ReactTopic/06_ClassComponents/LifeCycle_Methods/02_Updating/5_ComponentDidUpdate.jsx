d408f880f581594d918f10c01ca82a9024bd3d35
RAHUL <rahulmeetawa@gmail.com>
2023-06-05 16:44:15 +0530

    add ComponentDidUpdate

--- src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/5_ComponentDidUpdate.jsx
+++ src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/5_ComponentDidUpdate.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const ComponentDidUpdate = () => {
-  return <div></div>;
-};
+class ComponentDidUpdate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favoritecolor: "red" };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ favoritecolor: "yellow" });
+    }, 1000);
+  }
+  componentDidUpdate() {
+    document.getElementById("mydiv").innerHTML =
+      "The update fcolor is " + this.state.favoritecolor;
+  }
+  render() {
+    return (
+      <>
+        <h1>favoritecolor is : {this.state.favoritecolor}</h1>
+        <div id="mydiv"></div>
+      </>
+    );
+  }
+}
 
 export default ComponentDidUpdate;
