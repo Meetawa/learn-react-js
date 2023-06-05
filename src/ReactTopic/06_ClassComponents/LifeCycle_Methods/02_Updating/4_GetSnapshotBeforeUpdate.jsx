b28398df06799a4a6af291383993ac61059c2ff6
RAHUL <rahulmeetawa@gmail.com>
2023-06-05 16:28:51 +0530

    add GetSnapshotBeforeUpdate

--- src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/4_GetSnapshotBeforeUpdate.jsx
+++ src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/4_GetSnapshotBeforeUpdate.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 class GetSnapshotBeforeUpdate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fcolor: "red" };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ fcolor: "green" });
+    });
+  }
   render() {
     return <div>rahul ji</div>;
   }
