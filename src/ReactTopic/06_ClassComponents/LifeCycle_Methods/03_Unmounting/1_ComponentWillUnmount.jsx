7fd166af8de8a49e08237a46ea86893f9f0aa53b
RAHUL <rahulmeetawa@gmail.com>
2023-06-05 16:48:46 +0530

    ComponentWillUnmount

--- src/ReactTopic/06_ClassComponents/LifeCycle_Methods/03_Unmounting/1_ComponentWillUnmount.jsx
+++ src/ReactTopic/06_ClassComponents/LifeCycle_Methods/03_Unmounting/1_ComponentWillUnmount.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 
 class ComponentWillUnmount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: true };
+  }
+  delHeader = () => {
+    this.setState({ show: false });
+  };
   render() {
-    return <div>rahul saini</div>;
+    let myheader;
+    if (this.state.show) {
+      myheader = <Child />;
+    }
+    return (
+      <div>
+        {myheader}
+        <button type="button" onClick={this.delHeader}>
+          Delete Header
+        </button>
+      </div>
+    );
   }
 }
 
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Header is about to be unmounted.");
+  }
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
 export default ComponentWillUnmount;
