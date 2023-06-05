d5b4a3d88fc2375da996326204e967045394f343
RAHUL <rahulmeetawa@gmail.com>
2023-06-05 16:32:55 +0530

    add GetSnapshotBeforeUpdate

--- src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/4_GetSnapshotBeforeUpdate.jsx
+++ src/ReactTopic/06_ClassComponents/LifeCycle_Methods/02_Updating/4_GetSnapshotBeforeUpdate.jsx
@@ -8,10 +8,24 @@ class GetSnapshotBeforeUpdate extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       this.setState({ fcolor: "green" });
-    });
+    }, 1000);
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+      "Before the update, the favorite was " + prevState.fcolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+      "The updated favorite is " + this.state.fcolor;
   }
   render() {
-    return <div>rahul ji</div>;
+    return (
+      <div>
+        <h1>My Favorite Color is {this.state.fcolor}</h1>
+        <div id="div1"></div>
+        <div id="div2"></div>
+      </div>
+    );
   }
 }
 
