3d04f98566d9b8ec7d5b50ccdf22fc37b8061297
RAHUL <rahulmeetawa@gmail.com>
2023-06-06 15:17:26 +0530

    add synthetic event

--- src/ReactTopic/07_Events/04_SyntheticEvent.jsx
+++ src/ReactTopic/07_Events/04_SyntheticEvent.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const SyntheticEvent = () => {
-  return <div></div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default SyntheticEvent;
