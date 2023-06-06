6ea8145cdf3c4591c2901f6539460c22ff431a1c
RAHUL <rahulmeetawa@gmail.com>
2023-06-06 11:26:46 +0530

    add Content SimpleExample

--- src/ReactTopic/07_Events/01_SimpleExample.jsx
+++ src/ReactTopic/07_Events/01_SimpleExample.jsx
@@ -4,9 +4,17 @@ const SimpleExample = () => {
   const Successful = () => {
     alert("Form Successfully login");
   };
+
+  const Forget = () => {
+    document.write("password is changed");
+  };
+
   return (
     <>
+      {/* This is One way to handle events */}
       <button onClick={Successful}>Click Button </button>
+      {/* This is One way to handle events */}
+      <button onClick={Forget}>Forget password </button>
     </>
   );
 };
