e05874bfffa3a0ab84f5cdc6bff91afe00e158b0
RAHUL <rahulmeetawa@gmail.com>
2023-06-06 14:19:55 +0530

    add

--- src/ReactTopic/07_Events/EventIndex.js
+++ src/ReactTopic/07_Events/EventIndex.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SimpleExample from "./01_SimpleExample";
 import PassingArguments from "./02_PassingArguments";
+import ReactEventObject from "./03_ReactEventObject";
 
 const EventIndex = () => {
   return (
     <>
       <SimpleExample />
+      <br />
       <PassingArguments />
+      <br />
+      <ReactEventObject />
     </>
   );
 };
