b6a49cc716fd377f64dd9a0cfe0fdc18f44f8236
RAHUL <rahulmeetawa@gmail.com>
2023-06-06 14:25:41 +0530

    update react event objext

--- src/ReactTopic/07_Events/03_ReactEventObject.jsx
+++ src/ReactTopic/07_Events/03_ReactEventObject.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 const ReactEventObject = () => {
   const shoot = (a, b) => {
-    alert(b.type);
+    console.log(b);
+    console.log(b.type);
+    console.log(a);
     /*
         'b' represents the React event that triggered the function,
         in this case the 'click' event
         */
   };
 
-  return <button onClick={(event) => shoot("Goal!", event)}>click </button>;
+  return (
+    <>
+      <button onClick={(event) => shoot("Goal!", event)}>click </button>
+    </>
+  );
 };
 
 export default ReactEventObject;
