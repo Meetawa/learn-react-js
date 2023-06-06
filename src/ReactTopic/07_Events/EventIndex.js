cbd51b883059851ff13aba2244b63b78958d40c6
RAHUL <rahulmeetawa@gmail.com>
2023-06-06 15:24:56 +0530

    add event using class

--- src/ReactTopic/07_Events/EventIndex.js
+++ src/ReactTopic/07_Events/EventIndex.js
@@ -3,6 +3,7 @@ import SimpleExample from "./01_SimpleExample";
 import PassingArguments from "./02_PassingArguments";
 import ReactEventObject from "./03_ReactEventObject";
 import SyntheticEvent from "./04_SyntheticEvent";
+import EventUsingClass from "./06_EventUsingClass";
 
 const EventIndex = () => {
   return (
@@ -14,6 +15,8 @@ const EventIndex = () => {
       <ReactEventObject />
       <br />
       <SyntheticEvent />
+      <br />
+      <EventUsingClass />
     </>
   );
 };
