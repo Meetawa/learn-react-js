f8596d3aaedc659cf378ac246d61b60e265c0c83
RAHUL <rahulmeetawa@gmail.com>
2023-06-07 05:36:45 +0530

    add LogicalOperator

--- src/ReactTopic/08_Conditional_Rendering/ConditionalRendering.js
+++ src/ReactTopic/08_Conditional_Rendering/ConditionalRendering.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SimpleExample from "./01_SimpleExample";
+import LogicalOperator from "./02_LogicalOperator";
 
 const ConditionalRendering = () => {
+  const cars = ["Ford", "BMW", "Audi"];
   return (
     <>
       <SimpleExample isLoggedIn="true" />
+      <LogicalOperator cars={cars} />
     </>
   );
 };
