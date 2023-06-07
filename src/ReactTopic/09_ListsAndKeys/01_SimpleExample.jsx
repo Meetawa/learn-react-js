e25befc5021070749a310808adfcec16ea0e1d46
RAHUL <rahulmeetawa@gmail.com>
2023-06-07 15:24:20 +0530

    add all file

---
+++ src/ReactTopic/09_ListsAndKeys/01_SimpleExample.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SimpleExample = () => {
+  return (
+    <>
+      <h1>rahulsaini</h1>
+    </>
+  );
+};
+
+export default SimpleExam;
+<h1>rahulsaini</h1>;
---
+++ src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SimpleExam from "./01_SimpleExample";
+
+const ListAndKeys = () => {
+  return (
+    <div>
+      <SimpleExam />
+    </div>
+  );
+};
+
+export default ListAndKeys;
