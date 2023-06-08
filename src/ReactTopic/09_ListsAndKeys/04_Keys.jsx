b3403f591fea67ad24f1f082fdcc1dd189b22326
RAHUL <rahulmeetawa@gmail.com>
2023-06-08 21:46:39 +0530

    add keys

--- src/ReactTopic/09_ListsAndKeys/04_Keys.jsx
+++ src/ReactTopic/09_ListsAndKeys/04_Keys.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-
+//Using key
 const Keys = () => {
   const numbers = [1, 2, 3, 4, 5];
   const DoubleNumbers = numbers.map((num) => {
     return <li key={num.toString()}>{num * 10}</li>;
   });
+  const todos = [
+    {
+      text: "Rahul Saini",
+    },
+    {
+      text: "Ashish Saini",
+    },
+    {
+      text: "Madhu Saini",
+    },
+    {
+      text: "Sona Saini",
+    },
+    {
+      text: "Priyanka Saini",
+    },
+  ];
+  const todoItems = todos.map((todo) => <li key={todo.id}>{todo.text}</li>);
+  //When you don’t have stable IDs for rendered items, you may use the item index as a key as a last resort
+  const todoItemss = todos.map((todo, index) => (
+    // Only do this if items have no stable IDs
+    <li key={index}>{todo.text}</li>
+  ));
   return (
     <div>
       <ul>{DoubleNumbers}</ul>
+      <ul>{todoItems}</ul>
+      <ul>{todoItemss}</ul>
     </div>
   );
 };
