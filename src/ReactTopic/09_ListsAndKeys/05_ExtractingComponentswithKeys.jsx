1b6e49accb1b1ff6319a6dac014aa6906a882c63
RAHUL <rahulmeetawa@gmail.com>
2023-06-08 22:21:47 +0530

    add keys

--- src/ReactTopic/09_ListsAndKeys/05_ExtractingComponentswithKeys.jsx
+++ src/ReactTopic/09_ListsAndKeys/05_ExtractingComponentswithKeys.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const ExtractingComponentswithKeys = () => {
-  return <div></div>;
+/* function ListItem(props) {
+  const value = props.value;
+  return (
+    // Wrong! There is no need to specify the key here:
+    <li key={value.toString()}>{value}</li>
+  );
+}
+const ExtractingComponentswithKeys = (props) => {
+  const numbers = props.numbers;
+  const listItems = numbers.map((num) => {
+    <ListItem value={num} />;
+  });
+  return <ul>{listItems}</ul>;
+}; */
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+const ExtractingComponentswithKeys = (props) => {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => (
+    // Correct! Key should be specified inside the array.
+    <ListItem key={number.toString()} value={number} />
+  ));
+  return <ul>{listItems}</ul>;
 };
-
 export default ExtractingComponentswithKeys;
