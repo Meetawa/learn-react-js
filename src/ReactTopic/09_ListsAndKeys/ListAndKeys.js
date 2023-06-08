cdf7a670075b950d2397c467013d5a40a71cd1ae
RAHUL <rahulmeetawa@gmail.com>
2023-06-08 17:24:18 +0530

    add BasicListComponent

--- src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
+++ src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SimpleExam from "./01_SimpleExample";
 import RenderingMultipleComponents from "./02_RenderingMultipleComponents";
+import BasicListComponent from "./03_BasicListComponent";
 
 const ListAndKeys = () => {
   return (
     <div>
       <SimpleExam />
       <RenderingMultipleComponents />
+      <BasicListComponent />
     </div>
   );
 };
