dbe191fbaf8bacebbbce4939f584eac1d179051a
RAHUL <rahulmeetawa@gmail.com>
2023-06-08 16:46:07 +0530

    add RenderingMultipleComponents

---
+++ src/ReactTopic/09_ListsAndKeys/02_RenderingMultipleComponents.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RenderingMultipleComponents = () => {
+  return <div></div>;
+};
+
+export default RenderingMultipleComponents;
--- src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
+++ src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SimpleExam from "./01_SimpleExample";
+import RenderingMultipleComponents from "./02_RenderingMultipleComponents";
 
 const ListAndKeys = () => {
   return (
     <div>
       <SimpleExam />
+      <RenderingMultipleComponents />
     </div>
   );
 };
