cb6f9f984879f42db51cdb61dde1a4eba25b0501
RAHUL <rahulmeetawa@gmail.com>
2023-06-08 22:02:20 +0530

    add

--- src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
+++ src/ReactTopic/09_ListsAndKeys/ListAndKeys.js
@@ -3,6 +3,7 @@ import SimpleExam from "./01_SimpleExample";
 import RenderingMultipleComponents from "./02_RenderingMultipleComponents";
 import BasicListComponent from "./03_BasicListComponent";
 import Keys from "./04_Keys";
+import ExtractingComponentswithKeys from "./05_ExtractingComponentswithKeys";
 
 const ListAndKeys = () => {
   const numbers = [1, 2, 3, 4, 5];
@@ -12,6 +13,7 @@ const ListAndKeys = () => {
       <RenderingMultipleComponents />
       <BasicListComponent numbers={numbers} />
       <Keys />
+      <ExtractingComponentswithKeys numbers={numbers} />
     </div>
   );
 };
