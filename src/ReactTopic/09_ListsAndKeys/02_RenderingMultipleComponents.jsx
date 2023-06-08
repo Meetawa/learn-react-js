0fd723da903945fcf04f68021c8ddd34da236973
RAHUL <rahulmeetawa@gmail.com>
2023-06-08 17:13:33 +0530

    update RenderingMultipleComponents

--- src/ReactTopic/09_ListsAndKeys/02_RenderingMultipleComponents.jsx
+++ src/ReactTopic/09_ListsAndKeys/02_RenderingMultipleComponents.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const RenderingMultipleComponents = () => {
-  return <div></div>;
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number, key) => <li>{number}</li>);
+
+  console.log(numbers);
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  );
 };
 
 export default RenderingMultipleComponents;
