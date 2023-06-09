066d80ae74aeb724b2e601540c13a5e88546a269
RAHUL <rahulmeetawa@gmail.com>
2023-06-09 22:06:00 +0530

    add submitting forms

--- src/ReactTopic/10_Forms/FormIndex.js
+++ src/ReactTopic/10_Forms/FormIndex.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SimpleForm from "./01_SimpleForm";
 import HandlingForms from "./02_HandlingForms";
+import SubmittingForms from "./03_SubmittingForms";
 
 const FormIndex = () => {
   return (
     <>
       <SimpleForm />
       <HandlingForms />
+      <SubmittingForms />
     </>
   );
 };
