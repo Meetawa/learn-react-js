7a4870b4b45377fafac5d32d7d3e66c31b1d8222
RAHUL <rahulmeetawa@gmail.com>
2023-06-10 21:58:10 +0530

    add TextArea

--- src/ReactTopic/10_Forms/05_TextArea.jsx
+++ src/ReactTopic/10_Forms/05_TextArea.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TextArea = () => {
-  return <div></div>;
+  const [textarea, setTextarea] = useState(
+    "Rahul Saini is a Software Engineer,he is looking for a job "
+  );
+  const handleChange = (event) => {
+    setTextarea(event.target.value);
+    console.log(event.target.value);
+  };
+  return (
+    <>
+      <h1>TextArea :</h1>
+      <textarea value={textarea} onChange={handleChange} />
+    </>
+  );
 };
 
 export default TextArea;
