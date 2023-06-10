719346daa80cff85d5adc7691b9c6578ab97b1f6
RAHUL <rahulmeetawa@gmail.com>
2023-06-10 22:02:12 +0530

    Update Select file

--- src/ReactTopic/10_Forms/06_Select.jsx
+++ src/ReactTopic/10_Forms/06_Select.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Select = () => {
-  return <div></div>;
+  const [myCar, setMyCar] = useState("Volvo");
+  console.log(myCar);
+  const handleChange = (event) => {
+    setMyCar(event.target.value);
+  };
+
+  return (
+    <>
+      <h1> Select :</h1>
+      <form>
+        <select value={myCar} onChange={handleChange}>
+          <option value="Ford">Ford</option>
+          <option value="Volvo">Volvo</option>
+          <option value="Fiat">Fiat</option>
+        </select>
+      </form>
+    </>
+  );
 };
 
 export default Select;
