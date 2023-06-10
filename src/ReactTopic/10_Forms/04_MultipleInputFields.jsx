e11cf52f680f32c4b7b42f5329c7693b9fb8964a
RAHUL <rahulmeetawa@gmail.com>
2023-06-10 21:19:33 +0530

    add MultipleInputFields

--- src/ReactTopic/10_Forms/04_MultipleInputFields.jsx
+++ src/ReactTopic/10_Forms/04_MultipleInputFields.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MultipleInputFields = () => {
-  return <div></div>;
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  };
+  return (
+    <>
+      <h1>MultipleInputFields :</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            name="username"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Enter your age:
+          <input
+            type="number"
+            name="age"
+            value={inputs.age || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
+  );
 };
 
 export default MultipleInputFields;
