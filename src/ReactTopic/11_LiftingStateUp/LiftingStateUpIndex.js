b5bf820ff1e1081171b6975adb0c16f6a816de61
RAHUL <rahulmeetawa@gmail.com>
2023-06-16 13:41:12 +0530

    update LiftingStateUpIndex

--- src/ReactTopic/11_LiftingStateUp/LiftingStateUpIndex.js
+++ src/ReactTopic/11_LiftingStateUp/LiftingStateUpIndex.js
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
-import TodoCount from "./TodoApp/TodoCount";
-import TodoList from "./TodoApp/TodoList";
-import AddTodo from "./TodoApp/AddTodo";
+import React from "react";
 
 const LiftingStateUpIndex = () => {
-  const [todos, setTodos] = useState(["item 1", "item 2", "item 3"]);
-  return (
-    <>
-      <>
-        <TodoCount todos={todos} />
-        <TodoList todos={todos} />
-        <AddTodo setTodos={setTodos} />
-      </>
-    </>
-  );
+  return <div></div>;
 };
 
 export default LiftingStateUpIndex;
