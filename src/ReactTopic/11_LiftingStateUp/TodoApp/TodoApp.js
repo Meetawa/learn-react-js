e34cc9d16a920e2eb09086aaee1bc6d0f64ed35c
RAHUL <rahulmeetawa@gmail.com>
2023-06-16 13:40:31 +0530

    add todo app file

---
+++ src/ReactTopic/11_LiftingStateUp/TodoApp/TodoApp.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import TodoCount from "./TodoApp/TodoCount";
+import TodoList from "./TodoApp/TodoList";
+import AddTodo from "./TodoApp/AddTodo";
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState(["item 1", "item 2", "item 3"]);
+  return (
+    <>
+      <>
+        <TodoCount todos={todos} />
+        <TodoList todos={todos} />
+        <AddTodo setTodos={setTodos} />
+      </>
+    </>
+  );
+};
+
+export default TodoApp;
