448a1f0dd3d573f37096b0bcc72243365a22469b
RAHUL <rahulmeetawa@gmail.com>
2023-06-17 20:38:45 +0530

    add data

--- src/ReactTopic/11_LiftingStateUp/ChildA.jsx
+++ src/ReactTopic/11_LiftingStateUp/ChildA.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ChildB from "./ChildB";
 
 const ChildA = () => {
-  return <div></div>;
+  const nameIS = "Rahul Saini";
+  return (
+    <>
+      <ChildB nameIS={nameIS} />
+    </>
+  );
 };
 
 export default ChildA;
