cc1f4f22fa16e5c74dfd42970a8e8e74777c8da3
RAHUL <rahulmeetawa@gmail.com>
2023-06-18 16:16:30 +0530

    render a component

--- src/ReactTopic/05_Props/PropsIndex.js
+++ src/ReactTopic/05_Props/PropsIndex.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Fruit from "./01_FunctionComponents";
 import Car from "./02_ClassComponents";
+import Destructuring from "./03_Destructuring";
 
 const PropsIndex = () => {
   const fname = { name: "Mongo", color: "yellow" };
@@ -9,6 +10,7 @@ const PropsIndex = () => {
     <div>
       <Fruit details={fname} />
       <Car carDetails={Cname} />
+      <Destructuring active="KAPIL GARG" activeStatus="CSE" />
     </div>
   );
 };
