69e4896e3692bfae4fd89b26eccd1dd769318274
RAHUL <rahulmeetawa@gmail.com>
2023-06-18 15:27:42 +0530

    render a component

--- src/ReactTopic/12_Composition_vs_Inheritance/CompAndInhIndex.js
+++ src/ReactTopic/12_Composition_vs_Inheritance/CompAndInhIndex.js
@@ -1,7 +1,13 @@
 import React from "react";
+import InheritanceIndex from "./01_Inheritance/InheritanceIndex";
 
 const CompAndInhIndex = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1> Inheritance is : </h1>
+      <InheritanceIndex />
+    </>
+  );
 };
 
 export default CompAndInhIndex;
