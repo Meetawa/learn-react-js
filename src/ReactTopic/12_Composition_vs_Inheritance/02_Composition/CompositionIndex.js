1fffdef7351653c93038f7794ea52358d4bdd2c4
RAHUL <rahulmeetawa@gmail.com>
2023-06-18 15:37:48 +0530

    render a component

---
+++ src/ReactTopic/12_Composition_vs_Inheritance/02_Composition/CompositionIndex.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { CreateUserName, UpdateUserName } from "./Composition";
+
+const CompositionIndex = () => {
+  return (
+    <>
+      <CreateUserName />
+      <UpdateUserName />
+    </>
+  );
+};
+
+export default CompositionIndex;
