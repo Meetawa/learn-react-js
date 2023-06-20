10ec8e27e1d33b48ba2502448f33582e4bcfb321
RAHUL <rahulmeetawa@gmail.com>
2023-06-20 13:44:43 +0530

    add a function component name is arrow function

---
+++ src/01_Basic_React_Topics/00_React_ES6/02_ArrowFunction.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ArrowFunction = () => {
+  return <div>hey this is rahul saini</div>;
+};
+const ArrowFunctionWithProps = (val) => {
+  return <div>Age is is {val}</div>;
+};
+
+export { ArrowFunction, ArrowFunctionWithProps };
