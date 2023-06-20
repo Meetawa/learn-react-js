a8b4ae341fe0f544cbb4b798e09e5e9b05109ac0
RAHUL <rahulmeetawa@gmail.com>
2023-06-20 13:54:15 +0530

    Render variables Components

--- src/01_Basic_React_Topics/00_React_ES6/ReactES6Concept.js
+++ src/01_Basic_React_Topics/00_React_ES6/ReactES6Concept.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Classes from "./01_Classes";
 import { ArrowFunction, ArrowFunctionWithProps } from "./02_ArrowFunction";
+import Variables from "./03_Variables";
 
 const ReactES6Concept = () => {
   return (
@@ -8,9 +9,13 @@ const ReactES6Concept = () => {
       <h1>ReactES6Concept : </h1>
       <h2> 1.Classes :</h2>
       <Classes />
+
       <h2>2. ArrowFunction : </h2>
       <ArrowFunction />
       <ArrowFunctionWithProps val={"19"} />
+
+      <h2>3. Variables : </h2>
+      <Variables />
     </>
   );
 };
