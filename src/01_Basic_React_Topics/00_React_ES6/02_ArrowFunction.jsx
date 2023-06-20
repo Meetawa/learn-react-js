d9fafdbe6cc83a3afae353e358d4510cc173e8c3
RAHUL <rahulmeetawa@gmail.com>
2023-06-20 13:48:43 +0530

    add a function component name is arrow function

--- src/01_Basic_React_Topics/00_React_ES6/02_ArrowFunction.jsx
+++ src/01_Basic_React_Topics/00_React_ES6/02_ArrowFunction.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const ArrowFunction = () => {
   return <div>hey this is rahul saini</div>;
 };
-const ArrowFunctionWithProps = (val) => {
-  return <div>Age is is {val}</div>;
+const ArrowFunctionWithProps = (props) => {
+  console.log(props.val);
+  return <div>Age is :{props.val}</div>;
 };
 
 export { ArrowFunction, ArrowFunctionWithProps };
--- src/01_Basic_React_Topics/00_React_ES6/ReactES6Concept.js
+++ src/01_Basic_React_Topics/00_React_ES6/ReactES6Concept.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Classes from "./01_Classes";
+import { ArrowFunction, ArrowFunctionWithProps } from "./02_ArrowFunction";
 
 const ReactES6Concept = () => {
   return (
     <>
-      <h1>Classes :</h1>
+      <h1>ReactES6Concept : </h1>
+      <h2> 1.Classes :</h2>
       <Classes />
+      <h2>2. ArrowFunction : </h2>
+      <ArrowFunction />
+      <ArrowFunctionWithProps val={"19"} />
     </>
   );
 };
