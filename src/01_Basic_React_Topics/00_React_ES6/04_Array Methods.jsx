757dbfd9fa82f244afbee4e4872727eb2c799d34
RAHUL <rahulmeetawa@gmail.com>
2023-06-20 14:23:52 +0530

    add  a filter method

--- src/01_Basic_React_Topics/00_React_ES6/04_Array Methods.jsx
+++ src/01_Basic_React_Topics/00_React_ES6/04_Array Methods.jsx
@@ -6,10 +6,27 @@ const ArrayMethods = () => {
   const NewNumbers = Numbers.map((num) => {
     return num * 4;
   });
+  console.log(NewNumbers);
+
+  // 2.reduce()
+  const Values = [1, 2, 3, 4, 5];
+  const newValue = Values.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+  console.log(newValue);
+
+  // 3.filter()
+  const Data = [1, 2, 3, 4, 5, 6, 7];
+  const newData = Data.filter((number) => number >= 3);
+  console.log(newValue);
   return (
     <>
-      <h3>Map</h3>
+      <h3>4.1 Map( )</h3>
       {NewNumbers}
+      <h3>4.2 Reduce( )</h3>
+      {newValue}
+      <h3>4.3 Filter( )</h3>
+      {newData}
     </>
   );
 };
