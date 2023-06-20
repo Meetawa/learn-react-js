73e1b371a76ffbc5d6028724163391ee72398ec5
RAHUL <rahulmeetawa@gmail.com>
2023-06-20 13:41:38 +0530

    add a function component name is Classes

--- src/01_Basic_React_Topics/00_React_ES6/01_Classes.jsx
+++ src/01_Basic_React_Topics/00_React_ES6/01_Classes.jsx
@@ -10,10 +10,19 @@ const Classes = () => {
       return "I have a " + this.brand;
     }
   }
+  class Model extends Car {
+    constructor(name, mod) {
+      super(name);
+      this.model = mod;
+    }
+    show() {
+      return this.present() + ", it is a " + this.model;
+    }
+  }
 
-  const mycar = new Car("Ford");
-  mycar.present();
-  return <div></div>;
+  const mycar = new Model("Ford", "Mustang");
+
+  return <div>{mycar.show()}</div>;
 };
 
 export default Classes;
