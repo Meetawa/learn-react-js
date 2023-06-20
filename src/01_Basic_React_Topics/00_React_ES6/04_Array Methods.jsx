6b8aa0befeb7a1e6cc3323d3490439bd380667df
RAHUL <rahulmeetawa@gmail.com>
2023-06-20 15:21:43 +0530

    add a function component name is ArrayMethods

--- src/01_Basic_React_Topics/00_React_ES6/04_Array Methods.jsx
+++ src/01_Basic_React_Topics/00_React_ES6/04_Array Methods.jsx
@@ -19,6 +19,17 @@ const ArrayMethods = () => {
   const Data = [1, 2, 3, 4, 5, 6, 7];
   const newData = Data.filter((number) => number >= 3);
   console.log(newValue);
+
+  //4.includes()
+
+  const Datas = [1, 2, 3, 4, 5];
+  const newDatas = Datas.includes(3);
+
+  //5.find()
+  const num = [1, 2, 3, 4];
+  const newNum = num.find((number) => number > 3);
+  console.log(newValue);
+
   return (
     <>
       <h3>4.1 Map( )</h3>
@@ -27,6 +38,10 @@ const ArrayMethods = () => {
       {newValue}
       <h3>4.3 Filter( )</h3>
       {newData}
+      <h3>4.4 Includes( )</h3>
+      {console.log(newDatas)}
+      <h3>4.5 find( )</h3>
+      {newNum}
     </>
   );
 };
