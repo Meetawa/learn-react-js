0e5dd24e84bb46d6db1ce6fa1e5cf70748a4d13e
RAHUL <rahulmeetawa@gmail.com>
2023-06-21 22:01:16 +0530

    add a function component name is Modules

---
+++ src/01_Basic_React_Topics/00_React_ES6/08_Modules.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Modules = () => {
+  //go to this components , path : src\01_Basic_React_Topics\03_ImportAndExport\Readme.MD
+
+  return <div></div>;
+};
+
+export default Modules;
