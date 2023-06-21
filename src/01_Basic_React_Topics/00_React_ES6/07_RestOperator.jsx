750359f0b7239aa6fb88299797c9b67340cf53e1
RAHUL <rahulmeetawa@gmail.com>
2023-06-21 21:52:28 +0530

    Render rest Operator Components

--- src/01_Basic_React_Topics/00_React_ES6/07_RestOperator.jsx
+++ src/01_Basic_React_Topics/00_React_ES6/07_RestOperator.jsx
@@ -7,6 +7,19 @@ const RestOperator = () => {
   }
   console.log(fun(1, 2)); // 3
   console.log(fun(1, 2, 3, 4, 5)); //
+
+  // With rest parameter
+
+  function fun1(...input) {
+    let sum = 0;
+    for (let i of input) {
+      sum += i;
+    }
+    return sum;
+  }
+  console.log(fun1(1, 2)); //3
+  console.log(fun1(1, 2, 3)); //6
+  console.log(fun1(1, 2, 3, 4, 5)); //15
   return <div></div>;
 };
 
