76040ccdad842eb4636316ede149bf54a3dd21f3
RAHUL <rahulmeetawa@gmail.com>
2023-06-25 21:36:32 +0530

    Render a update Component

--- src/02_React_Hooks/01_useState_Hook/UseStateIndex.js
+++ src/02_React_Hooks/01_useState_Hook/UseStateIndex.js
@@ -1,11 +1,14 @@
 import React from "react";
 import DeclareState from "./01_DeclareState";
+import UpdateState from "./02_UpdateState";
 
 const UseStateIndex = () => {
   return (
     <>
       <h1>Declaration of useState : </h1>
       <DeclareState />
+      <h1>UpdateState :</h1>
+      <UpdateState />
     </>
   );
 };
