a00a67568e06d2bf36e897f910ec812715b51342
RAHUL <rahulmeetawa@gmail.com>
2023-06-25 21:22:20 +0530

    Render a declaration Component

--- src/02_React_Hooks/01_useState_Hook/UseStateIndex.js
+++ src/02_React_Hooks/01_useState_Hook/UseStateIndex.js
@@ -1,7 +1,13 @@
 import React from "react";
+import DeclareState from "./01_DeclareState";
 
 const UseStateIndex = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>Declaration of useState : </h1>
+      <DeclareState />
+    </>
+  );
 };
 
 export default UseStateIndex;
