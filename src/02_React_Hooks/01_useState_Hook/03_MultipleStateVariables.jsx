3ec7978560caea3b135c6be50522781946224ed2
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 15:19:21 +0530

    add a MultipleStateVariables component

--- src/02_React_Hooks/01_useState_Hook/03_MultipleStateVariables.jsx
+++ src/02_React_Hooks/01_useState_Hook/03_MultipleStateVariables.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MultipleStateVariables = () => {
-  return <div></div>;
+  const [age, setAge] = useState(19);
+  const [siblingsNum, setSiblingsNum] = useState(10);
+
+  const handleAge = () => setAge(age + 1);
+  const handleSiblingsNum = () => setSiblingsNum(siblingsNum + 1);
+
+  return (
+    <div>
+      <p>Today I am {age} Years of Age</p>
+      <p>I have {siblingsNum} siblings</p>
+
+      <div>
+        <button onClick={handleAge}>Get older!</button>
+        <button onClick={handleSiblingsNum}>More siblings!</button>
+      </div>
+    </div>
+  );
 };
 
 export default MultipleStateVariables;
