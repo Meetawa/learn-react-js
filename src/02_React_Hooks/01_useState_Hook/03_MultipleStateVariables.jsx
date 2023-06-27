af7e7b52d3407561cf7437bd2b05caff4ff80102
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 15:29:24 +0530

    add a MultipleStateVariables component

--- src/02_React_Hooks/01_useState_Hook/03_MultipleStateVariables.jsx
+++ src/02_React_Hooks/01_useState_Hook/03_MultipleStateVariables.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+/* 
+
 const MultipleStateVariables = () => {
   const [age, setAge] = useState(19);
   const [siblingsNum, setSiblingsNum] = useState(10);
@@ -15,6 +17,33 @@ const MultipleStateVariables = () => {
       <div>
         <button onClick={handleAge}>Get older!</button>
         <button onClick={handleSiblingsNum}>More siblings!</button>
+      </div>
+    </div>
+  );
+};
+
+*/
+
+//Create a single Hook that holds an object:
+const MultipleStateVariables = () => {
+  const [state, setState] = useState({ age: 19, siblingsNum: 4 });
+  const handleClick = (val) =>
+    setState({
+      ...state,
+      [val]: state[val] + 1,
+    });
+  const { age, siblingsNum } = state;
+
+  return (
+    <div>
+      <p>Today I am {age} Years of Age</p>
+      <p>I have {siblingsNum} siblings</p>
+
+      <div>
+        <button onClick={handleClick.bind(null, "age")}>Get older!</button>
+        <button onClick={handleClick.bind(null, "siblingsNum")}>
+          More siblings!
+        </button>
       </div>
     </div>
   );
